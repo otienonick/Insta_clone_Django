3940603292ce75fab8c326d36a3f5abdeb234ca3
otienonick <nick.odongi@student.moringaschool.com>
2021-10-16 17:51:55 +0300

    added comment form

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile,Comment
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
@@ -14,4 +14,15 @@ class ProfileUpdateForm(forms.ModelForm):
         fields = [
             'profile_image',
             'bio',
+        ]
+
+class CommentForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('Comment','Comment',css_class = 'btn-success'))
+
+    class Meta:
+        model = Comment
+        fields = [
+            'body',
         ]

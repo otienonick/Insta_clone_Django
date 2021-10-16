dd01cc2ab9c86c3082acb69e52f542562bfd2661
otienonick <nick.odongi@student.moringaschool.com>
2021-10-16 16:44:10 +0300

    created my profile class and post class

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Profile(models.Model):
+    profile_image = CloudinaryField('image', null=True,default = 'default.jpg')
+    bio = models.TextField(default="")
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+        
+class Post(models.Model):
+    author = models.ForeignKey(User,on_delete = models.CASCADE)
+    image = CloudinaryField('image', null=True)
+    caption = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now) 
+    likes = models.IntegerField(default=0)
+    location = models.CharField(max_length=80, null=True)
+
+
+    @classmethod
+    def search_by_location(cls,search_term):
+        photos = cls.objects.filter(location__icontains = search_term)   
+        # We filter the model data using the __icontains query filter
+        return photos    
+   
+
+    def __str__(self):
+        return self.caption        

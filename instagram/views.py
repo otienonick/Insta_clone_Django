d437946213b76d4429416b908127c310e54495f9
otienonick <nick.odongi@student.moringaschool.com>
2021-10-16 17:03:24 +0300

    added post and profile view functions

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import  Post
+from django.utils import timezone
+from .forms import ProfileUpdateForm
+
 
 # Create your views here.
+def post(request):
+    posts = Post.objects.all().filter(created_date__lte = timezone.now()).order_by('-created_date')
+
+
+    return render(request, 'insta/post.html', {'posts':posts})
+
+def profile(request):
+    user = request.user
+    posts = Post.objects.filter(author=request.user).order_by('-created_date')
+
+    if request.method == 'POST':
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)
+
+        if p_form.is_valid():
+            p_form.save()
+            # messages.success(request,f'your account has been updated')
+
+            return redirect('profile')
+
+
+    else:
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+
+    return render(request, 'insta/profile.html', {'p_form':p_form,"posts" : posts,'user' : user})    
